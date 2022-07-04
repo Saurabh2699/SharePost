367a44dece6a6e2c3ebcec9b2fdce290f0fbc15c
saurabh khaparey <saurabhkhaparey05@gmail.com>
2022-07-04 22:54:26 +0530

    resolved push conflicts

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    let decodedToken;
+
+    if (token) {
+      decodedToken = jwt.verify(token, process.env.SECRET);
+      req.userId = decodedToken?.id;
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
